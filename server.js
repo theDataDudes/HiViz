86d15ad863709f7451279ba4fbd05d00be449557
Alexander Anich <alexander.anich@gmail.com>
2015-11-07 15:57:30 -1000

    add inert, server index.html
    -- add npm module inert
    -- make relative path to public directory in hapi server
    -- serve index.html
    
    references : having to fix serve index.html to angular

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 'use strict';
 const PORT = process.env.PORT || 3000;
 const Hapi = require('hapi');
-const server = new Hapi.Server();
-
-server.connection({ port : PORT });
-
-// serves index for angular
-server.route({
-  method : 'GET',
-  path : '/',
-  handler : (request, reply) => {
-    reply.file('./public/views/index.html');
+const Path = require('path');
+const Inert = require('inert');
+const server = new Hapi.Server({
+  connections : {
+    routes : {
+      files : {
+        relativeTo : Path.join(__dirname, 'public')
+      }
+    }
   }
 });
 
-server.start( () => {
-  console.log('Serving running on port: ' + PORT);
+server.connection({ port : PORT });
+
+server.register(Inert, function () {
+
+  // serves index for angular
+  server.route({
+    method : 'GET',
+    path : '/{path*}',
+    handler : {
+      file : 'views/index.html'
+    }
+  })
+
+  server.start( () => {
+    console.log('Serving running on port: ' + PORT);
+  });
 });
