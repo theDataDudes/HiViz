dc74d82490263effe88bc1d9f62467398e717bed
Alexander Anich <alexander.anich@gmail.com>
2015-11-07 17:59:18 -1000

    fix static serving on hapi server.js
    -- added secondary route to handle other static files besides
       index.html
    
    resolve : fix static server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3000;
 const Hapi = require('hapi');
 const Path = require('path');
 const Inert = require('inert');
+
 const server = new Hapi.Server({
   connections : {
     routes : {
@@ -15,18 +16,29 @@ const server = new Hapi.Server({
 
 server.connection({ port : PORT });
 
-server.register(Inert, function () {
+server.register(Inert, () => {});
 
-  // serves index for angular
-  server.route({
-    method : 'GET',
-    path : '/{path*}',
-    handler : {
-      file : 'views/index.html'
+// serves index for angular
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        file: 'views/index.html'
     }
-  })
+});
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler : {
+        directory : {
+            path : '.',
+            redirectToSlash : true,
+            index : true
+        }
+    }
+});
 
-  server.start( () => {
-    console.log('Serving running on port: ' + PORT);
-  });
+server.start( (err) => {
+  if (err) throw err;
+  console.log('Serving running on port: ' + PORT);
 });
