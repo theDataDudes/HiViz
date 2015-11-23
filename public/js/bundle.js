ce3b0b76a4f407daddfe725de9c93001c2112d15
Brock <brocklanoza@cupofjava.local>
2015-11-23 09:40:00 -1000

    added bio for Brock

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2435,7 +2435,7 @@ angular.module('app', [
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
 }]);
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_3f5f67d2.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_56331a22.js","/")
 },{"./c3-charts":6,"./common":19,"./main":23,"./sideCharts":25,"./sidebar":26,"buffer":2,"rH1JPG":4}],23:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 'use strict';
